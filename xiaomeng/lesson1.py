1c320577483439584366c190029efeebb987e01c
xiaomeng <1772337473@qq.com>
2019-04-06 13:16:51 +0800

    xiaomeng代码上传

---
+++ xiaomeng/lesson1.py
@@ -0,0 +1,15 @@
+﻿def judgenames():
+    py = input("请输入您的变量名:")
+    while 1:
+        if py.isalpha() and py == "_":
+            print("您输入的变量名合法")
+            if py.isalnum()
+                print("您输入的变量名合法")
+            else:
+                print("您输入的变量名不合法")
+            break
+        else:
+            print("您输入的变量名不合法")
+if __name__=="__main__":
+    judgenames()
+    
