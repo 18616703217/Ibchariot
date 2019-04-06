714748a250a02f0d69c99b611cc107ccc1924e6d
zhenglijing <zhengijingone@live.com>
2019-04-06 11:22:01 +0800

    ljfunc.py

---
+++ zlj/ljfunc.py
@@ -0,0 +1,15 @@
+﻿#encoding = utf-8
+
+def func(var)
+    if var[0].isalpha() or var[0] == "_":
+        for i in var[1:]:
+            if not(i.isalnum() or i == "_"):
+                return("%s变量名不合法" %s)
+            else:
+                return("%s变量名合法" %s)
+    else:
+        print("%s变量名不合法" %s)
+
+if __name__ ="__main__":
+    func(judgenames)
+        
