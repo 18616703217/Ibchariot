795a07a73a01dbf5c36fc272fab11fa10b25fe0e
Your Name <459619994@qq.com>
2019-04-06 11:19:17 +0800

    1.py

---
+++ judgenames/1.py
@@ -0,0 +1,25 @@
+﻿def func():
+    a=input("请输入变量名:")
+    if a[0].isalpha() or a[0] == "_":
+        if a[1::].isalnum() or a[1::] == "_":
+            print("该变量名合法")
+        else:
+            print("该变量名中间内容不合法")
+    else:
+        print("该变量名开头不合法")
+func()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
