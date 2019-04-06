5be4f04ce1041f2b90ac5d780b97a3977583d15e
wenqiz <1300994936@qq.com>
2019-04-06 13:31:43 +0800

    规则断言BUG修复

--- JiahaoZ/judgenames.py
+++ JiahaoZ/judgenames.py
@@ -1,15 +1,16 @@
 ﻿#encoding utf-8
-def func(a):
-    while 1:
-        if a[0] in b or a[0] == "_":
-            print("您输入的变量名合法")
-            
-        else:
-            print("您输入的变量名不合法")
-        break
+def func(variable_value):
+    if variable_value[0] not in start_rule:
+        return "变量名不符合开头只能用字母下划线要求"
+    for i in variable_value[1:]:
+        if i not in end_rule:
+            return "变量名不符合内容只能用字母数字下划线"
+    return "变量合法"
 
 if __name__=="__main__":
     import string
-    b=string.ascii_letters
-    a=str(input("请输入变量名: "))
-    func(a)
+    start_rule = string.ascii_letters+"_"
+    end_rule = string.ascii_letters+"_"+"".join([str(i) for i in range(10)])
+    variable_value=input("请输入变量名: ")
+    assert_rule_retsult = func(variable_value)
+    print("%s 断言规则结果为：%s" % (variable_value,assert_rule_retsult))
